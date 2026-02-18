d6d20a551f562c99250c927c80e7fdcb04eb15af
Flinn69 <flinn9669@gmail.com>
2026-02-18 07:55:34 +0500

    Time: 0 ms (100.00%) | Memory: 20.7 MB (6.95%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,12 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        #left начало 
+        #right конец 
         left, right = 0, len(nums) - 1
+        #пока левая граница не пересекла правую
         while left <= right:
+            #находит середину текущего диапазона
             mid = (left + right) // 2
+            #если элемент в середине это то что надо 
             if nums[mid] == target:
-                return mid
+                return mid  #возвращает его индекс
+            #если искомое число больше, чем число в середине
             elif nums[mid] < target:
-                left = mid + 1
+                left = mid + 1  #сдвигает левую границу вправо
+            #если число меньше, ищет слева
             else:
-                right = mid - 1
+                right = mid - 1  #сдвигает правую границу влево
+        #если число не найдено, возвращает -1
         return -1
