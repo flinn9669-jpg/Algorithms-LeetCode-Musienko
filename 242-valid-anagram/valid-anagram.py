acab9da77c93b55cd0fd69a56f23e7e9a6debbfa
Flinn69 <flinn9669@gmail.com>
2026-03-19 14:52:34 +0500

    Time: 11 ms (76.01%) | Memory: 19.2 MB (98.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #если длины разные, то сразу не анаграммы
         if len(s) != len(t):
-            return False  #быстро отсекает разные длины
-        count = defaultdict(int)  #словарь
-        #считает буквы в s
+            return False
+        count = {}  #хэш-таблица
+        #считает символы из строки s
         for char in s:
-            count[char] += 1
-        #вычитает буквы из t
+            count[char] = count.get(char, 0) + 1
+        #вычитает символы из строки t
         for char in t:
-            if char not in count or count[char] == 0:
+            if char not in count:
                 return False
             count[char] -= 1
-        return True  #все буквы совпали
+        #проверяет, что все значения равны 0
+        for value in count.values():
+            if value != 0:
+                return False
+        return True
